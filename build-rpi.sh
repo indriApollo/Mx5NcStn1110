4526ee317e6581f7184b555391b1638c90ff0533
indriApollo <indri.apollo@gmail.com>
2024-04-04 22:35:07 +0200

    feat: rpi build script

---
+++ build-rpi.sh
@@ -0,0 +1,1 @@
+dotnet publish DigitalDash -c Release -o publish -r linux-arm64 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
